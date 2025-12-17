aa90263708814e5951eec0fa95c960f4f0e65946
srmora-ec <alvarezjoseandres@outlook.es>
2025-12-17 00:45:33 -0500

    Prueba definitiva

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ async def webhook(request: Request):
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Hola mundi!"}
+    return {"Hello": "Hola mundo, prueba definitiva!"}
 
 # CRUD automático para el modelo Producto
 router = SQLAlchemyCRUDRouter(
